b01909b208d0f2cd4c1547cb44b805dbf583c5ca
Eugênio Moreira <eugeniojava@outlook.com>
2022-07-23 15:34:23 -0300

    feat: setup project

---
+++ src/main/java/com/eugeniojava/javaspringcleanarchitectureexample/JavaSpringCleanArchitectureExampleApplication.java
@@ -0,0 +1,11 @@
+package com.eugeniojava.javaspringcleanarchitectureexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCleanArchitectureExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringCleanArchitectureExampleApplication.class, args);
+    }
+}
